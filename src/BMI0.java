05854dd63bb885e3b2a940b28c0a885f7209f18e
Carson Robison <carson.robison.2019@student.lhschools.org>
2018-09-13 08:44:27 -0400

    final

--- src/BMI0.java
+++ src/BMI0.java
@@ -5,18 +5,25 @@ public class BMI0 {
 
     public static void main (String[] args) {
 
-    String weight,height;
+    
+    float Total,Problem,Problem2,weight,height;
 
 
     Scanner Radar;
     Radar = new Scanner(System.in);
 
     System.out.println("Hello and welcome to the BMI Calculator");
-        System.out.println("First can I get you height in meters leave out the Units");
-        height = Radar.nextLine();
-        System.out.println("First can I get you weight in KG leave out the Units");
-        weight = Radar.nextLine();
-        System.out.println("Your BMI is "+ (height/weight));
+        System.out.println("First can I get you height in inches ");
+        height = Radar.nextInt();
+        System.out.println("First can I get you weight in Pounds ");
+        weight = Radar.nextInt();
+        Problem = weight/ height;
+        System.out.println(Problem);
+        Problem2 = Problem/height;
+        System.out.println(Problem2);
+        Total = Problem2*703;
+        System.out.println("Your BMI is "+Total);
+
 
 
 
